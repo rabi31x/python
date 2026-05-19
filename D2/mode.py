b2438942f89a614e810af49f4056026cb885f9a6
rabi31x <rabbit135pok@gmail.com>
2026-05-19 15:44:02 +0900

    mode.py

---
+++ D2/mode.py
@@ -0,0 +1,16 @@
+# 케이스 t값
+# 1000개 score 리스트
+# 0~100 점  count = [0] *100
+t = int(input())
+for i in range(1, t+1):
+    tc = int(input())
+    score = map(int, input().split())
+    count = [0] * 101
+    for score_num in score:
+        count[score_num] += 1
+    max_count = max(count)
+    for j in range(100, -1, -1):
+        if count[j] == max_count:
+            mode_num = j
+            break
+    print(f"#{tc} {mode_num}")
