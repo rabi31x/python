4e5e211f9f13fb3ab63b603bcb8dee68482757aa
rabi31x <rabbit135pok@gmail.com>
2026-05-19 09:51:29 +0900

    D2 sumodd

--- D2/sum_odd.py
+++ D2/sum_odd.py
@@ -1,3 +1,13 @@
-print("Hello World")
-print("Hello World")
-print("Hello World")
+t = int(input())
+data = []
+result = 0
+for case in range(1, t+1):
+    data = list(map(int, input().split()))
+    result_list = []
+    result = 0
+    for j in range(len(data)):
+        if data[j] % 2 == 1:
+            result_list.append(data[j])
+    result = sum(result_list)
+    print(f"#{case} {result}")
+
